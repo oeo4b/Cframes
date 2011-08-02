f9d293d059dfa79ce9bb3bbf9ff862e6b13eb483
oeo4b <omar@omar.com>
2011-08-02 21:46:52 +0200

    created struct with modes for each file type

---
+++ src/extensions.h
@@ -0,0 +1,20 @@
+#ifndef EXTENSIONS_H
+#define EXTENSIONS_H
+
+struct extension {
+  bool bin;
+  char *type;
+  char *http;  
+} extensions [] = {
+  {true, (char *)"gif", (char *)"image/gif"},
+  {true, (char *)"jpg", (char *)"image/jpeg"},
+  {true, (char *)"jpeg", (char *)"image/jpeg"},
+  {true, (char *)"png", (char *)"image/png"},
+  {false, (char *)"htm", (char *)"text/html"},
+  {false, (char *)"html", (char *)"text/html"},
+  {false, (char *)"txt", (char *)"text/html"},
+  {false, (char *)"zip", (char *)"images/zip"},
+  {0, 0, 0}  
+};
+
+#endif
