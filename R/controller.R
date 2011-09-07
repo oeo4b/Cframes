0375ee72db8d1e8ae40f9a47d88c5e04ff6b4cb1
omar <omar@omar.com>
2011-09-06 21:14:05 -0400

    expanded some generic functions

--- R/controller.R
+++ R/controller.R
@@ -1,3 +1,25 @@
+# Base class for all controllers
 setClass("Controller",
   representation(id="character", other="numeric")
+)
+
+setGeneric("index",
+  function(controller, ...) 
+    standardGeneric("index")
+)
+setGeneric("create",
+  function(controller, ...) 
+    standardGeneric("create")
+)
+setGeneric("read",
+  function(controller, ...) 
+    standardGeneric("read")
+)
+setGeneric("update",
+  function(controller, ...) 
+    standardGeneric("update")
+)
+setGeneric("destroy",
+  function(controller, ...) 
+    standardGeneric("destroy")
 )
